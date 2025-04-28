0cfaffc2e86527ba5c222d8659afbc787a59f92a
Anonymous-2003 <avishekbasyal3@gmail.com>
2025-04-28 12:08:26 +0545

    structuring

--- Backend/server.js
+++ Backend/server.js
@@ -4,15 +4,12 @@ import { connectDB } from "./config/db_connect.js";
 import todoRoute from "./routes/todo.route.js";
 import userRoute from "./routes/user.route.js";
 
-
-// middlewares
-app.use(express.json()); // allows us to accept JSON data in the req.body
-
-
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 4001;
+
+app.use(express.json()); // allows us to accept JSON data in the req.body
 
 
 app.use("/todo", todoRoute); //"For any route that starts with /todo, use the routes defined in todoRoute."
